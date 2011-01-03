2335b5e675beb83a953427aa60f0d1c69e1f5f5f
Kevin Wright <kev.lee.wright@gmail.com>
2011-01-03 19:53:31 +0000

    starting work on types

--- src/main/scala/scalaj/reflect/Mirrors.scala
+++ src/main/scala/scalaj/reflect/Mirrors.scala
@@ -28,18 +28,44 @@ sealed trait Mirror {
   }
 }
 
-case class ClassMirror(sym: ClassSymbol) extends Mirror {
+abstract class ModuleMirror(sym: Symbol) extends Mirror {
+  private[this] val CONSTRUCTOR_NAME = "<init>"
+  
+  def classSymbol: ClassSymbol
+  override def childSymbols = classSymbol.children
+  def constructors = children.collect{ case m: MethodMirror if m.name  == CONSTRUCTOR_NAME => m }
+  def innerObjects = children.collect{ case o: ObjectMirror => o }
+  def innerClasses = children.collect{ case c: ClassMirror => c }
+  def innerModules = innerObjects ++ innerClasses
+  def methods = children.collect{ case m: MethodMirror if m.name  != CONSTRUCTOR_NAME => m}
+
+  override def printTree(indent: Int = 0) : Unit = {
+    println(("  " * indent) + toString)
+    constructors foreach {_.printTree(indent+1)}
+    innerObjects foreach {_.printTree(indent+1)}
+    innerClasses foreach {_.printTree(indent+1)}
+    methods foreach {_.printTree(indent+1)}
+  }
+
+}
+
+case class ClassMirror(sym: ClassSymbol) extends ModuleMirror(sym) {
+  val classSymbol = sym
   override def toString = "class " + name
   //def methods: Seq[MethodMirror] =
 }
 
-case class ObjectMirror(sym: ObjectSymbol) extends Mirror {
-  private[this] val TypeRefType(prefix, classSymbol: ClassSymbol, typeArgs) = sym.infoType
-  override def childSymbols = classSymbol.children
-
+case class ObjectMirror(sym: ObjectSymbol) extends ModuleMirror(sym) {
+  val TypeRefType(prefix, classSymbol: ClassSymbol, typeArgs) = sym.infoType
   override def toString = "object " + name
 }
 
 case class MethodMirror(sym: MethodSymbol) extends Mirror {
   override def toString = "def " + name
+  override def printTree(indent: Int = 0) : Unit = {
+    println(("  " * indent) + toString)
+  }
 }
+
+
+
---
+++ src/main/scala/scalaj/reflect/TypeMirrors.scala
@@ -0,0 +1,4 @@
+package scalaj.reflect
+
+
+class TypeMirrors
